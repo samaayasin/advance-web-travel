7580fbae30b87396fccd393d80912a447ea473aa
Ahmad <nazzala8@gmail.com>
2024-08-19 19:32:23 +0300

    before swagger

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function login()
+    {
+        return "test";
+    }
+
+    public function register(Request $request)
+    {
+
+        // Validation Rules
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:3|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        // Handling Validation Errors
+        if ($validator->fails()) {
+            return response()->json([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        $request->validate(['name' => 'required|string|max:7']);
+
+        $user = User::all();
+        return $request->name;
+    }
 }
