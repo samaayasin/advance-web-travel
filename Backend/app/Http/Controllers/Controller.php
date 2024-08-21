46fca119cd8067af0cd2f98120d632180472245e
Aisha2-eng <aishaishtayeh112@gmail.com>
2024-08-21 14:34:35 +0300

    Backend

--- Backend/app/Http/Controllers/Controller.php
+++ Backend/app/Http/Controllers/Controller.php
@@ -1,7 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
-
+/**
+ * @OA\Info(
+ *     title="Booking Management System API",
+ *     version="1.0.0"
+ * )
+ */
 abstract class Controller
 {
     //
---
+++ Backend/app/Models/Flight.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Flight extends Model
+{
+    use HasFactory;
+
+    protected $table = 'flights';
+
+    protected $primaryKey = 'FlightID';
+
+    protected $fillable = [
+        'UserID',
+        'AirlineName',
+        'DepartureAirport',
+        'ArrivalAirport',
+        'DepartureTime',
+        'ArrivalTime',
+        'Price',
+        'image_url'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'UserID', 'UserID');
+    }
+
+    public function bookingFlights(){
+        return $this->hasMany(BookingFlight::class, 'FlightID', 'FlightID');
+
+    }
+    
+}
