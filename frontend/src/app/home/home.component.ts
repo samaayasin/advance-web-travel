cb2e7e171f4dcf7f0c3acca22f89c5f886e9d904
Aisha2-eng <aishaishtayeh112@gmail.com>
2024-09-01 23:07:11 +0300

    navbar & home page

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+import AOS from 'aos';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  ngOnInit() {
+    AOS.init();
+  }
+}
