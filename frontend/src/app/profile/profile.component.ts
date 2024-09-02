414f165a62872dd1df95bef36793ce499d2114a4
Ahmad <nazzala8@gmail.com>
2024-09-02 10:57:46 +0300

    update profile page

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ProfileService } from '../services/profile.service';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { FlightService } from '../services/flight.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,22 +11,46 @@ import { AuthService } from '../services/auth.service';
 })
 export class ProfileComponent implements OnInit {
   user: any;
+  flights: any[] = [];
 
-  constructor(private profileService: ProfileService, private router: Router, private authService:AuthService){}
-  
+  constructor(private flightService: FlightService,private profileService: ProfileService, private router: Router, private authService:AuthService){}
   ngOnInit(): void {
-    this.profileService.getUser()?.subscribe({
-      next: (response) => {
-        this.user = response
-      },
-      error: (error) => {
-        alert("Your session is expired relogin again")
-        this.router.navigate(['/sign-in'])
-        this.authService.logout();
-        console.log(error);
-        
-      }
-    });
+    const userObservable = this.profileService.getUser();
+    if (userObservable) {
+      userObservable.subscribe({
+        next: (response) => {
+          if (response) {
+            console.log(response);
+            
+            this.user = response;
+    
+            this.flightService.getFlightsForUser(response.UserID).subscribe({
+              next: (flightsResponse) => {
+                console.log(flightsResponse);
+                this.flights = flightsResponse;
+              },
+              error: (error) => {
+                console.error("Error fetching flights: ", error);
+              }
+            });
+          } else {
+            alert("Failed to retrieve user data. Please try again.");
+            this.router.navigate(['/sign-in']);
+            this.authService.logout();
+          }
+        },
+        error: (error) => {
+          alert("Your session has expired. Please relogin.");
+          this.router.navigate(['/sign-in']);
+          this.authService.logout();
+          console.error("Error fetching user data: ", error);
+        }
+      });
+    } else {
+      alert("Failed to retrieve user data. Please try again.");
+      this.router.navigate(['/sign-in']);
+      this.authService.logout();
+    }
     
   }
 
