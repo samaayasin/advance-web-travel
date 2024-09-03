03fbca41230b7c83c642e630651a075d64092b78
samaayasin <samaayasin85@gmail.com>
2024-09-03 16:51:52 +0300

    update

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Frontend';
+  showNavbar = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const hiddenRoutes = ['/admin', '/Summary', '/flights', '/cars','/hotels','/bookingflight','/bookinghotel','/bookingcar','/users'];
+        this.showNavbar = !hiddenRoutes.some(route => event.url.includes(route));
+      }
+    });
+  }
 }
